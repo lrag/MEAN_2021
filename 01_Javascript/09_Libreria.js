6f3e09fcfc5d6106239222fa74ec08da7260efb7
lrag <a@b.c>
2021-02-16 13:15:28 +0100

    Programando la libreria JS definitiva

--- 01_Javascript/09_Libreria.js
+++ 01_Javascript/09_Libreria.js
@@ -46,9 +46,83 @@ function rellenarFormulario(objeto){
     }
 }
 
+//Parámetros obligatorios
+//-array de objetos.
 //
-//
-//
-function generarTabla(){
+//Parámetros opcionales:
+//{
+//    columnas    : [],
+//    propiedades : [],
+//    idTabla     : "",
+//    class       : "", 
+//    onclick     : function(obj que ocupa la fila en la que se ha hecho click){}
+//}
+function generarTabla(objetos, parametrosOpcionales){
+
+    //Comprobamos si vienen los nombres de las columnas
+    //Si no vienen se utilizan los nombres de TODAS las propiedades del 
+    //primer objeto que haya en 'objetos''
+    let nombresColumnas    = []   
+    let nombresPropiedades = [] 
+    if(parametrosOpcionales && parametrosOpcionales.columnas){
+        nombresColumnas = parametrosOpcionales.columnas
+        nombresPropiedades = parametrosOpcionales.propiedades
+    } else {
+        let primerObjeto = objetos[0]
+        for(let propiedad in primerObjeto){
+            let x = propiedad.slice(0,1).toUpperCase()+propiedad.slice(1)
+            nombresColumnas.push(x)
+            nombresPropiedades.push(propiedad)
+        }
+    }
+
+    //Comprobamos si viene el id para la tabla
+    let idTabla = null
+    if( parametrosOpcionales && parametrosOpcionales.idTabla){
+        idTabla = parametrosOpcionales.idTabla
+    } else {
+        idTabla = "tabla-"+Date.now()
+    }
+
+    //Comprobamos si viene la funcion para el onclick
+    let onclick = null
+    if( parametrosOpcionales && parametrosOpcionales.onclick){
+        onclick = parametrosOpcionales.onclick
+    }
+
+    ////////////////////
+    //Creamos la tabla//
+    ////////////////////
+
+    let tabla = $(`<table id="${idTabla}" border="1" align="center"></table>`)
+    //Creamos la cabecera
+    let trCabecera = $(`<tr></tr>`)
+    //Añadimos los TH correspondientes
+    $(nombresColumnas).each(function(){
+        trCabecera.append(`<th>${this}</th>`)
+    })
+    tabla.append(trCabecera)
+    
+    //En este bucle recorremos los objetos y creamos los tr
+    $(objetos).each(function(pos, obj){
+        let tr = $("<tr>")
+        //En este bucle recorremos las propiedades y creamos los td
+        $(nombresPropiedades).each(function(pos, nombrePropiedad){
+            $(`<td>${obj[nombrePropiedad]}</td>`).appendTo(tr)
+        })
+
+        //Si nos han pasado el onclick se lo asignamos al tr
+        if(onclick){
+            tr.click(function(){
+                $(`#${idTabla} tr`).css('background-color', 'white')
+                this.style.background = 'lightGreen'
+                onclick(obj)
+            })
+        }
+        tr.appendTo(tabla)
+    })
+
+    return tabla
 
 }
+
