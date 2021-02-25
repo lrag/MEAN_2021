19b835aecb14b5aca7180a56dae1aa2fe99816e1
lrag <a@b.c>
2021-02-25 10:21:38 +0100

    Ya estaba asi cuando llegue

--- 04_Angular/Ej01-Angular/src/app/app.component.ts
+++ 04_Angular/Ej01-Angular/src/app/app.component.ts
@@ -19,9 +19,32 @@ export class AppComponent {
   //Asociaremos las cajas de texto del formulario a las propiedades de este objeto:
   //Nos aseguramos de que la propiedad tenga valor aqui o en el constructor
   public disco:Disco = new Disco()
+
+  //Declaramos un array aqui para los discos. No es el lugar correcto
+  public discos:Disco[] = []
   
   public insertarDisco():void{
     console.log("Insertar disco:",this.disco)
+    this.discos.push(this.disco)
+    console.log(this.discos)
+    this.vaciarFormulario()
+  }
+
+  public vaciarFormulario():void{
+    console.log("Vaciando el formulario...")
+    //No necesitamos buscar las cajas de texto y vaciarlas, pero podríamos hacerlo en caso de tener el día tonto
+    
+    //Como el formuario está unido al disco si modificamos las propiedades del objeto
+    //el cambio se ve en el formulario
+    
+    //Podemos hacerlo propiedad a propiedad...
+    //this.disco.id = null
+    //this.disco.titulo = ""
+    //this.disco.grupo = ""
+    //this.disco.year = null
+
+    //Podemos poner un disco nuevo y lo lo hacemos del tirón
+    this.disco = new Disco()
   }
 
 }
