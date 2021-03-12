d910c145e7697fe0378e08c83b987aa5675b090d
lrag <a@b.c>
2021-03-12 11:44:01 +0100

    Validando

--- 05_MEAN/01_Tienda_1.0/Node/negocio/negocioUsuarios.js
+++ 05_MEAN/01_Tienda_1.0/Node/negocio/negocioUsuarios.js
@@ -1,8 +1,24 @@
-
+//npm install validatorjs
+const validator = require('validatorjs')
 const mongoDBUtil = require("../util/MongoDBUtil")
 
+let reglasUsrInsercion = {
+    nombre  : 'required|min:3|max:40',
+    login   : 'required|min:5|max:15',
+    pw      : 'required|min:5|max:15',
+    correoE : 'required|email',
+}
 
 exports.altaUsuario = function(usuario){
+
+    let validador = new Validator(usuario, reglasUsrInsercion)
+    if(validador.fails()){
+        console.log(validador.errors.errors)
+        return
+    }
+    console.log(ok)
+
+    //
 
 
 
