53c6cb91762216d5b4c27d688c8f466942bb2764
lrag <a@b.c>
2021-03-15 13:23:16 +0100

    HTTPS

--- 05_MEAN/01_Tienda_1.0/Node/aplicacion.js
+++ 05_MEAN/01_Tienda_1.0/Node/aplicacion.js
@@ -1,4 +1,6 @@
+const https = require('https')
 const http = require('http')
+const fs = require('fs')
 const express = require('express')
 const mongoDBUtil = require('./util/MongoDBUtil')
 const usuariosRouter = require('./rest/usuariosRest').router
@@ -10,14 +12,31 @@ mongoDBUtil.conectarBBDD()
 })
 
 function arrancarServidor(){
-    let app = express()
-   
-    app.use(express.json());
 
+    //Configuramos express
+    let app = express()   
+    app.use(express.json());
     app.use(usuariosRouter)
 
     console.log("Arrancando el servidor...")
-    http.createServer(app).listen(6001, function(){
-        console.log("Esperando peticiones en el puerto 6001")
+    //Creamos el server utilizando el módulo 'https' y le proporcionamos
+    //la función express y el certificado
+    let cert = {
+        key  : fs.readFileSync("./certificado/server.key"),
+        cert : fs.readFileSync("./certificado/server.cert") 
+    }
+    https.createServer(cert, app).listen(6001, function(){
+        console.log("Esperando peticiones https en el puerto 6001")
     })
+
+    //Abrimos el puerto 80 para redirigir a https
+    http.createServer(function(request, response){
+        response.writeHead(301, {
+            Location : 'https://localhost:6001/'+request.url
+          });
+        response.end();    
+    }).listen(80, function(){
+        console.log("Esperando peticiones http en el puerto 80")
+    })    
+
 }
