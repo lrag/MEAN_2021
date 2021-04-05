d98250b210b68ecca3a675645593171a8204076e
lrag <a@b.c>
2021-04-05 09:57:58 +0200

    Lunes de resurreccion

--- 05_MEAN/02_Tienda_2.0/Angular/src/app/servicios/sessionService.ts
+++ 05_MEAN/02_Tienda_2.0/Angular/src/app/servicios/sessionService.ts
@@ -3,22 +3,32 @@ import { Injectable } from "@angular/core"
 @Injectable( { providedIn : 'root' } )
 export class SessionService {
 
-    private items:[] = []
+    //private items:[] = []
 
-    public setItem(clave:string, valor:any):void{
-        this.items[clave] = valor
+    public setItem(clave:string, valor:any, persistente:boolean=false):void{
+        //this.items[clave] = valor
+        if(persistente){
+            localStorage.setItem(clave,JSON.stringify(valor))
+        } else {
+            sessionStorage.setItem(clave, JSON.stringify(valor))
+        }
     }
 
     public getItem(clave:string):any{
-        return this.items[clave]
+        //return this.items[clave]
+        return JSON.parse(sessionStorage.getItem(clave))
+
+        
     }
 
     public removeItem(clave:string):void{
-        delete this.items[clave]
+        //delete this.items[clave]
+        sessionStorage.removeItem(clave)
     }
 
     public clear():void{
-        this.items = []
+        //this.items = []
+        sessionStorage.clear()
     }
     
 }
