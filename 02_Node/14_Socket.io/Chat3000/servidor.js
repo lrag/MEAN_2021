a1f3218a660c3b9534dc32fb7037677775dcf302
lrag <a@b.c>
2021-04-19 10:25:09 +0200

    Fistro y Fistra

--- 02_Node/14_Socket.io/Chat3000/servidor.js
+++ 02_Node/14_Socket.io/Chat3000/servidor.js
@@ -8,9 +8,9 @@ const express = require("express")
 //Configuramos el servidor web para entregar 'cliente.html'
 let app = express()
 app.use(express.static("./recursos"))
-/*app.listen(80, function(){
+app.listen(80, function(){
     console.log("Esperando peticiones HTTP en el puerto 80")
-})*/
+})
 
 //Socket.io no es una implementación de websockets (aunque lo utiliza y se parece mucho)
 //
@@ -19,19 +19,19 @@ app.use(express.static("./recursos"))
 //
 //Creamos un objeto server normal y corriente
 
-//const servidor = http.createServer(function(request,response){})
-const servidor = http.createServer(app)
+const servidor = http.createServer(function(request,response){})
+//const servidor = http.createServer(app)
 
 //Una vez que disponemos de un objeto server escuchando en un puerto
 //podemos crear la infraestructura de Socket.io
 const io = socketIO(
-    servidor/*,
+    servidor,
     {
         cors: {
-            origin: "http://localhost:80",
+            origin: "*",
             methods: ["GET", "POST"]
         }
-    }*/);
+    });
     
 servidor.listen(8000, function(){
     console.log("Esperando conexiones Socket.io en el puerto 8000")
@@ -59,36 +59,35 @@ let aliasUsuarios = []
 //distintos clientes
 //Cuando se conecta uno nuevo nos pasan el socket por parámetro por si
 //queremos hacer algo con el (queremos)
-io.on('connection', function(socket){
 
+io.on('connection', function(socket){
     console.log("Nueva conexión")
 
-    socket.on("alias", function(alias){
-        //Estas funciones se asignan al socket
-        //console.log(this)
-        console.log("Alias recibido:"+alias)
-        aliasUsuarios.push(alias)
-        console.log(aliasUsuarios)
-        //Enviamos los alias a todos los que estén conectados
-        io.emit("aliasUsuarios", JSON.stringify(aliasUsuarios))
-    })
-
+    socket.on("alias", aliasRecibido)
     socket.on("mensaje", nuevoMensaje)
-
-    socket.on("KE_ME_BOI_EH",function(alias){
-        console.log("Se va a ir:"+alias)
-        for(let a=0;a<aliasUsuarios.length;a++){
-            if(aliasUsuarios[a]==alias){
-                aliasUsuarios.slice(a,1)
-                break;
-            }
-        }
-    });
-
-    socket.on("disconnect", function(){
-        console.log("Usuario desconectado")
-    })
+    socket.on("disconnect", usuarioDesconectado)
 })
+
+function aliasRecibido(alias){
+    //Estas funciones se asignan al socket
+    //console.log(this)
+    console.log("Alias recibido:"+alias)
+    
+    //Comprobamos que el alias no esté repetido
+    let aliasRepetido = aliasUsuarios.find( elem => elem==alias )
+    if(aliasRepetido){
+        //Dentro de estas funciones 'this' es el socket
+        this.emit('aliasRepetido','pues eso')
+        return
+    }
+
+    aliasUsuarios.push(alias)
+    console.log(aliasUsuarios)
+    //Asociamos el alias al socket. Dentro de estas funciones 'this' es el socket
+    this.alias = alias
+    //Enviamos los alias a todos los que estén conectados
+    io.emit("aliasUsuarios", JSON.stringify(aliasUsuarios))
+} 
 
 function nuevoMensaje(mensaje){
     console.log("Mensaje recibido:"+mensaje)
@@ -99,3 +98,30 @@ function nuevoMensaje(mensaje){
     //Para emitir un mensaje a todos los sockets que haya
     io.emit("mensaje", mensaje)
 }
+
+function usuarioDesconectado(){
+    console.log("Usuario desconectado:"+this.alias)
+    
+    for(let a=0;a<aliasUsuarios.length;a++){
+        //En esta función 'this' es el socket
+        if(aliasUsuarios[a]==this.alias){
+            aliasUsuarios.splice(a,1)
+            break;
+        }
+    }  
+    //Avisamos a todo el mundo de que la lista de usuarios ha cambiado    
+    io.emit("aliasUsuarios", JSON.stringify(aliasUsuarios))
+}
+
+/*
+    socket.on("KE_ME_BOI_EH",function(alias){
+        console.log("Se va a ir:"+alias)
+        for(let a=0;a<aliasUsuarios.length;a++){
+            if(aliasUsuarios[a]==alias){
+                aliasUsuarios.splice(a,1)
+                break;
+            }
+        }
+    });
+*/
+
